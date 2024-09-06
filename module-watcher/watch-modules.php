28806ec76fa1be7df5c0ed2e90bdf32e9c85a4a7
Thrive Support <support@thriveagency.com>
2024-09-06 18:33:53 -0400

    Update watch-modules.php

--- module-watcher/watch-modules.php
+++ module-watcher/watch-modules.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Watch Modules
 Description: Module Watcher
-Version: 2.6
+Version: 1.0
 Author: Thrive Agency
 Author URI: https://thriveagency.com
 GitHub Plugin URI: https://github.com/Thrive-Agency/Module-Watcher
